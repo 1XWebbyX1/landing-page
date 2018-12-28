517afb60002c53fcf503bdb14065f5a6df4f13a0
Khayati Asrani <devinc@TheDen-3.local>
2018-12-29 00:04:50 +0530

    Updates

---
+++ precache-manifest.e902685897f985af7210193f522afd8d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "79e00e6938491484ff29",
+    "url": "/quote-machine-pwa/static/css/main.d95fe68d.chunk.css"
+  },
+  {
+    "revision": "79e00e6938491484ff29",
+    "url": "/quote-machine-pwa/static/js/main.79e00e69.chunk.js"
+  },
+  {
+    "revision": "147b6c0d946721962781",
+    "url": "/quote-machine-pwa/static/js/1.147b6c0d.chunk.js"
+  },
+  {
+    "revision": "719969c887df25e0931d",
+    "url": "/quote-machine-pwa/static/js/2.719969c8.chunk.js"
+  },
+  {
+    "revision": "d6ec3ce4dacf2952c59a",
+    "url": "/quote-machine-pwa/static/js/3.d6ec3ce4.chunk.js"
+  },
+  {
+    "revision": "742a4318d18960f9aee6",
+    "url": "/quote-machine-pwa/static/js/4.742a4318.chunk.js"
+  },
+  {
+    "revision": "5f4d562fd897a7a26b3d",
+    "url": "/quote-machine-pwa/static/js/runtime~main.5f4d562f.js"
+  },
+  {
+    "revision": "25dd9682e785ac2f423eeceab3073635",
+    "url": "/quote-machine-pwa/static/media/stars.25dd9682.svg"
+  },
+  {
+    "revision": "bbea91ac300fd27dbc62f4d6465624e4",
+    "url": "/quote-machine-pwa/static/media/side_img.bbea91ac.jpg"
+  },
+  {
+    "revision": "b7913b8c3206bdc513fa5ab4a465103b",
+    "url": "/quote-machine-pwa/static/media/front_img.b7913b8c.jpg"
+  },
+  {
+    "revision": "57b6a4046f53d24493cc05341ffbc38c",
+    "url": "/quote-machine-pwa/static/media/handmade.57b6a404.jpg"
+  },
+  {
+    "revision": "73ecbd874a78584cf6362ccf2e7b5f99",
+    "url": "/quote-machine-pwa/static/media/designer.73ecbd87.jpg"
+  },
+  {
+    "revision": "f6650660d897ae32efa38a437896d4a7",
+    "url": "/quote-machine-pwa/static/media/casual.f6650660.jpg"
+  },
+  {
+    "revision": "7aa23f77fd415550e36d22b03e3b06cb",
+    "url": "/quote-machine-pwa/static/media/handbag.7aa23f77.svg"
+  },
+  {
+    "revision": "31c6064774a294f68e5a066740307a64",
+    "url": "/quote-machine-pwa/static/media/handbag_back.31c60647.jpg"
+  },
+  {
+    "revision": "cce9e3c1b30d2ce84f306198cdd51e16",
+    "url": "/quote-machine-pwa/static/media/handbag_front.cce9e3c1.jpg"
+  },
+  {
+    "revision": "d835745afdbdbc14d5d7ea3f9d988f5a",
+    "url": "/quote-machine-pwa/index.html"
+  }
+];
