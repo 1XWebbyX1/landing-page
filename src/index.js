9d1aa9e2b4d4c661abed5ea1d6e85970679cf78a
Khayati Asrani <devinc@TheDen-3.local>
2019-01-08 16:25:09 +0530

    support ie

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import $ from 'jquery'
+import 'babel-polyfill';
 require('jquery-mousewheel');
 
 ReactDOM.render(<App />, document.getElementById('root'));
